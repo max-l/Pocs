19406a7debb66ba4f6ef431da7e76f1b21591e86
Maxime Lévesque <maxime.levesque@gmail.com>
2011-12-02 18:30:42 -0500

    Introduced non numeric types and CanEqual constraints

--- src/main/scala/poc/Poc1.scala
+++ src/main/scala/poc/Poc1.scala
@@ -1,8 +1,10 @@
 
 package poc
 
-trait TARRoot
-trait TAROptionBigDecimal extends TARRoot
+import java.util.Date
+
+trait TARNumeric
+trait TAROptionBigDecimal extends TARNumeric
 trait TARBigDecimal extends TAROptionBigDecimal with TARNonOption
 
 trait TAROptionDouble extends TAROptionBigDecimal
@@ -23,6 +25,16 @@ trait TARByte extends TAROptionByte with TARInt with TARNonOption
 trait TAROption extends TAROptionByte with TAROptionInt with TAROptionFloat with TAROptionLong with TAROptionDouble with TAROptionBigDecimal
 trait TARNonOption
 
+trait TARNonNumeric
+
+trait TARString extends TARNonNumeric with TAROptionString
+trait TARDate extends TARNonNumeric with TAROptionDate
+
+trait TAROptionString extends TARNonNumeric
+trait TAROptionDate extends TARNonNumeric
+
+
+trait CanEqual[-A1,-A2]
 
 object Impls {
 
@@ -38,9 +50,38 @@ object Impls {
   implicit def fi2b5(f: Option[Double]) = s7.create(f)
   implicit def fi2b6(f: Option[BigDecimal]) = s11.create(f)
   implicit def fi2b7(f: BigDecimal) = s12.create(f)
+  
+  implicit def snn1c(s: String) = snn1.create(s)
+  implicit def snn2c(s: Option[String]) = snn2.create(s)
+    
+  implicit val snn1 = new TypedExpressionFactory[String,TARString] {
+    def create(v: String) = new ConstantTypedExpression[String,TARString](v)
+    def convert(v: TypedExpression[_,_]) = new TypedExpressionConversion[String,TARString](v,this)
+    def sample = "": String
+  }
+  
+  implicit val snn2 = new TypedExpressionFactory[Option[String],TAROptionString] {
+    def create(v: Option[String]) = new ConstantTypedExpression[Option[String],TAROptionString](v)
+    def convert(v: TypedExpression[_,_]) = new TypedExpressionConversion[Option[String],TAROptionString](v,this)
+    def sample = Option("")
+  }
 
-  //implicit def v2b[A,B](a: A)(implicit bf: TypedExpressionFactory[A,B]): TypedExpression[A,B] = bf.create(a)
+  implicit val snnd1 = new TypedExpressionFactory[Date,TARDate] {
+    def create(v: Date) = new ConstantTypedExpression[Date,TARDate](v)
+    def convert(v: TypedExpression[_,_]) = new TypedExpressionConversion[Date,TARDate](v,this)
+    def sample = new Date
+  }
 
+  implicit val snnd2 = new TypedExpressionFactory[Option[Date],TAROptionDate] {
+    def create(v: Option[Date]) = new ConstantTypedExpression[Option[Date],TAROptionDate](v)
+    def convert(v: TypedExpression[_,_]) = new TypedExpressionConversion[Option[Date],TAROptionDate](v,this)
+    def sample = Option(new Date)
+  }
+  
+  implicit def snn4c(s: Option[Date]) = snnd2.create(s)
+  implicit def snn3c(s: Date) = snnd1.create(s)
+  
+  
   implicit val s1 = new TypedExpressionFactory[Byte,TARByte] {
     def create(v: Byte) = new ConstantTypedExpression[Byte,TARByte](v)
     def convert(v: TypedExpression[_,_]) = new TypedExpressionConversion[Byte,TARByte](v,this)
@@ -152,26 +193,46 @@ object Impls {
           b: TypedExpression[A2,E2])
          (implicit bs: OptionTypedExpressionFactory[A3,E4,E3]): TypedExpression[A3,E4] = bs.related.convert(a)
 
+  implicit val ce1 = new CanEqual[TARNumeric, TARNumeric] {}         
+  implicit val ce2 = new CanEqual[TARDate, TARDate] {}
+  implicit val ce3 = new CanEqual[TAROptionString, TAROptionString] {}
 }
 
 trait TypedExpression[A,F1] {
 
-  def add[F2 >: F1 <: TARRoot, E <: F2, B1, B2]
+  def add[F2 >: F1 <: TARNumeric, E <: F2, B1, B2]
          (b: TypedExpression[B1,E])
          (implicit bs: TypedExpressionFactory[B2,F2]): TypedExpression[B2,F2] = bs.convert(b)
 
 
-  def div[F2 >: F1 <: TARRoot, E <: F2, B1, B2, Z1, Z2]
+  def div[F2 >: F1 <: TARNumeric, E <: F2, B1, B2, Z1, Z2]
          (b: TypedExpression[B1,E])
          (implicit bs: TypedExpressionFactory[B2,F2],
                    tf: Floatifier[F2,Z1,Z2]): TypedExpression[Z1,Z2] = tf.convert(b)
 
   def s: A
+  
+  def ===[B,F2](b: TypedExpression[B,F2])(implicit ev: CanEqual[F1, F2]) = 0
 }
 
 class ConstantTypedExpression[A,B](val a: A) extends TypedExpression[A,B] {
   def s = a
 }
+
+
+object Tests2 {
+  import Impls._
+
+  1 === 1
+
+  "" === ""
+    
+  Option("") === ""
+  "" === Option("")
+  
+  1 === Option(1)
+}
+
 
 class TypedExpressionConversion[A,B](val b: TypedExpression[_,_], bf: TypedExpressionFactory[A,B]) extends TypedExpression[A,B] {
   def s = bf.sample
